3385bff33f6d6603ffceb2fe51338dec5ee23965
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-05 13:52:52 +0700

    Pemilihan2Percobaan2No3

--- Pemilihan2Percobaan2No3.java
+++ Pemilihan2Percobaan2No3.java
@@ -11,14 +11,12 @@ public class Pemilihan2Percobaan2No3 {
         sudut3 = input03.nextInt();
         totalSudut = sudut1 + sudut2 + sudut3;
 
-        if (totalSudut == 180) {
-            if ((sudut1 == 99) || (sudut2 == 90) || (sudut3 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-        } else 
-        System.out.println("Bukan segitiga");
-        
-
+        if (sudut1 == sudut2 && sudut2 == sudut3) {
+            System.out.println("Segitiga adalah segitiga sama sisi.");
+        } else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3) {
+            System.out.println("Segitiga adalah segitiga sama kaki.");
+        } else {
+            System.out.println("Segitiga adalah segitiga sembarang.");
+        }
     }
-}
+    }
