e00111fedbdf14e91290b4f8b4e5e176ed20a07d
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-05 12:44:44 +0700

    Pemilihan2Percobaan1No3_soalno3

--- Pemilihan2Percobaan1No3.java
+++ Pemilihan2Percobaan1No3.java
@@ -5,8 +5,8 @@ public class Pemilihan2Percobaan1No3 {
         System.out.println("Masukkan Tahun");
         int tahun = input03.nextInt();
         
-        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
-            if ((tahun % 100) != 0)
+        if ((tahun % 4 == 0) || (tahun % 400 == 0)) {
+            if ((tahun % 100) == 0)
             System.out.println("Tahun Kabisat");
 
         } else 
